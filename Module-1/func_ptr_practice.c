7260136486290658cebd686e893b97a0d65b4dc1
Salvador Rodriguez <chava.crb@gmail.com>
2024-09-03 12:05:21 -0600

    Update func ptr practice

--- Module-1/func_ptr_practice.c
+++ Module-1/func_ptr_practice.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /* This function is called when student has a non probatory grade */
-void mad (char * name, int grade)
+void mad(char *name, int grade)
 {
-  printf("Hey %s, you are a Bad boy you better study\n" 
+  printf("Hey %s, you are a Bad boy you better study\n"
          "your grade is %d!\n",
-          name, grade);
-  
+         name, grade);
+
   printf("Calling parents... \n");
-  for(int x = 0; x<3; x++)
+  for (int x = 0; x < 3; x++)
   {
     printf("ring\n");
     sleep(2); // this funcion is inside unistd.h!!
@@ -18,22 +18,22 @@ void mad (char * name, int grade)
 }
 
 /* This is the regular funcion for any normal student with a normal grade */
-void normal (char * name, int grade)
+void normal(char *name, int grade)
 {
   printf("Hey %s, Your grade is %d!\n",
          name, grade);
 }
 
 /* Use this funcion to refer to the BEST of the BEST of your class */
-void happy (char * name, int grade)
+void happy(char *name, int grade)
 {
   printf("Congrats! %s, Your grade is %d you are the best!\n",
          name, grade);
 }
 
-/* RETO: 
+/* RETO:
   El metodo main, debe de crear un arreglo de 3 puntos a funcion.
-  Por convencion el arreglo en la posicion MAD, tendra el puntero a la llamada 
+  Por convencion el arreglo en la posicion MAD, tendra el puntero a la llamada
   de funcion necesaria para la reprimenda (bad).
   En la posicion NORMAL tendra la llamada estandar (normal).
   Y en la posicion BEST tendra la llamada para los alumnos maravillos (happy).
@@ -53,33 +53,40 @@ Aprovecha los siguientes defines */
 */
 
 //.. int process_student_grade(..
-int process_student_grade(void (*func[])(char *, int), char *name, int grade)
-{
-  if(grade == 100)
-    func[BEST](name, grade);
-  
-  if(grade < 60)
-    func[MAD](name, grade);
-  else
-    func[NORMAL](name, grade);
 
+typedef void (*greeting)(char *name, int grade);
+int process_student_grade(greeting *arr, char *student, int grade)
+{
+  if (grade == 100)
+    arr[BEST](student, grade);
+  else if (grade >= 60)
+  {
+    greeting func_ptr = arr[NORMAL];
+    func_ptr(student, grade);
+  }
+  else if (grade <= 59)
+  {
+    greeting burro = arr[MAD];
+    burro(student, grade);
+  }
   return 0;
 }
 
-  
 int main()
 {
-    char student[10] = "Ken";
+  greeting messages[3];
+  messages[MAD] = mad;
+  messages[NORMAL] = normal;
+  messages[BEST] = happy;
+
+  char student[10] = "Ken";
   int grade;
   printf("Enter your grade... ");
   int ret = scanf("%d", &grade);
 
   /* TODO call process_student_grade
    process_student_grade (....*/
-  void (*func[])(char *, int) = {mad, normal, happy};
-  process_student_grade(func, student, grade);
-
-
+  process_student_grade(messages, student, grade);
 
   return 0;
 }
