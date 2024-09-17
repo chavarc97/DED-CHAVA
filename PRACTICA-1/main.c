953770e62b76a699022deb9822620b12f5e296e0
Salvador Rodriguez <chava.crb@gmail.com>
2024-09-16 18:41:10 -0600

    Practica 1: Add Game loop

--- PRACTICA-1/main.c
+++ PRACTICA-1/main.c
@@ -32,9 +32,9 @@ void clear_screen();
 int is_critical_hit(Player *defender, int dice_roll_result);
 void select_magic(Player *player);
 void select_magic_for_cpu(Player *cpu);
-int game_menu(Player *player);
-void select_action(Player *player, Player *defender, int choice);
-void magic_menu(Player *player, Player *defender);
+void game_loop(Player *human, Player *cpu);
+void player_turn(Player *player, Player *opponent);
+void cpu_turn(Player *cpu, Player *human);
 
 // function prototypes for the magic attacks
 void fireball(void *attacker, void *opponent);
@@ -51,161 +51,149 @@ int main()
 {
     // set the random number generator for the dice rolls (20 sided dice)
     srand(time(NULL));
-
     // create the players
     Player players[2];
-
-    /* ===================================================================================== */
     // Set the players and their magic attacks here....
     set_player(players);
     getchar();
-    /* ===================================================================================== */
     clear_screen();
-
-    /* ===================================================================================== */
-    // start game loop here...
-    int game_status = 1;
-
-    while (game_status)
-    {
-        // get the human player and the cpu player
-        Player *human = players;
-        Player *cpu = (players + 1);
-
-        // Check if either player has lost before continuing
-        if (human->hp <= 0)
-        {
-            printf("CPU wins!\n");
-            game_status = 0;
-            break;
-        }
-        if (cpu->hp <= 0)
-        {
-            printf("Human wins!\n");
-            game_status = 0;
-            break;
-        }
-
-        // Human player's turn
-        printf("Human's turn\n");
-        int choice = game_menu(human);
-        select_action(human, cpu, choice);
-        getchar();
-        printf("%s's hp: %d\n", human->name, human->hp);
-        printf("%s's hp: %d\n", cpu->name, cpu->hp);
-        clear_screen();
-        getchar();
-        // Check if the CPU has lost after the human's turn
-        if (cpu->hp <= 0)
-        {
-            printf("Human wins!\n");
-            game_status = 0;
-            break;
-        }
-
-        // CPU's turn
-        printf("CPU's turn\n");
-        choice = rand() % 3 + 1;
-        select_action(cpu, human, choice);
-
-        // Check if the human has lost after the CPU's turn
-        if (human->hp <= 0)
-        {
-            printf("CPU wins!\n");
-            game_status = 0;
-            break;
-        }
-
-        // Clear the screen between turns
-        clear_screen();
-    }
-
+    // Start the game loop
+    game_loop((players), (players + 1));
     return 0;
+}
+
+//================================================================================================
+// Functions
+void game_loop(Player *human, Player *cpu)
+{
+    int turn = 0;      // 0 for human, 1 for CPU
+    int game_over = 0; // 0 for game in progress, 1 for game over
+    int round = 1;
+    while (!game_over)
+    {
+        clear_screen();
+        printf("=== Round %d ===\n", round);
+        // show game state (HP of players)
+        printf("=== Battle Progress ===\n");
+        printf("%s: HP = %d \n\tDEFENSE = %d\n", human->name, human->hp, human->defense);
+        printf("CPU: HP = %d \n\tDEFENSE = %d\n", cpu->hp, cpu->defense);
+        printf("===========================\n\n");
+        // check if the human or cpu have won
+        if (human->hp <= 0)
+        {
+            printf("%s has been defeated! CPU wins!\n", human->name);
+            game_over = 1;
+            break;
+        }
+        else if (cpu->hp <= 0)
+        {
+            printf("CPU has been defeated! %s wins!\n", human->name);
+            game_over = 1;
+            break;
+        }
+        // alternate turns between human and CPU
+        if (turn == 0)
+        {
+            human->defense = 10; // Reset defense to 10 at the start of the turn
+            printf("It's your turn, %s\n", human->name);
+            player_turn(human, cpu);
+            round++;
+            turn = 1;
+        }
+        else
+        {
+            cpu->defense = 10; // Reset defense to 10 at the start of the turn
+            printf("It's CPU's turn\n");
+            cpu_turn(cpu, human);
+            round++;
+            turn = 0;
+        }
+
+        printf("Press enter to continue...\n");
+        getchar();
+    }
+}
+
+void player_turn(Player *player, Player *opponent)
+{
+    int choice;
+    printf("SELECT AN ACTION:\n");
+    printf("\t1. Attack\n");
+    printf("\t2. Defend\n");
+    printf("\t3. Use Magic\n");
+    printf("\tOpción: ");
+    scanf("%d", &choice);
+    getchar();
+    switch (choice)
+    {
+        case 1:
+            attack(player, opponent);
+            break;
+        case 2:
+            // Defend: increase defense by 5
+            player->defense += 5;
+            printf("%s is defending\n", player->name);
+            break;
+        case 3:
+        {
+
+            // Use magic: select a magic attack
+            clear_screen();
+            printf("Select a magic attack:\n");
+            for (int i = 0; i < 3; i++)
+            {
+                printf("\t%d. %s\n", i + 1, *(player->magic_name + i));
+            }
+            int m_choice;
+            printf("Opción: ");
+            scanf("%d", &m_choice);
+            getchar();
+            if (m_choice >= 1 && m_choice <= 3)
+            {
+                magic_func magic = *(player->magic + m_choice - 1);
+                magic(player, opponent);
+            }
+            else
+            {
+                printf("Invalid choice\n");
+            }
+        }
+        break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+}
+void cpu_turn(Player *cpu, Player *human)
+{
+    int action = rand() % 3 + 1; // Randomly select an action for the CPU
+    switch (action)
+    {
+        case 1:
+            attack(cpu, human);
+            break;
+        case 2:
+            // Defend: increase defense by 5
+            cpu->defense += 5;
+            printf("CPU is defending\n");
+            break;
+        case 3:
+        {
+            // Use magic: randomly select a magic attack
+            int m_choice = rand() % 3;
+            magic_func magic = *(cpu->magic + m_choice);
+            magic(cpu, human);
+        }
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
 }
 
 void clear_screen()
 {
     printf("\033[H\033[J"); // Clear the screen
-}
-int game_menu(Player *player)
-{
-    int choice;
-    printf("Player: %s\n", player->name);
-    printf("Choose an action: \n");
-    printf("1. Attack\n");
-    printf("2. Use magic\n");
-    printf("3. Defend\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-    return choice;
-}
-void select_action(Player *player, Player *defender, int choice)
-{
-    if (strcmp(player->name, "CPU") == 0)
-    {
-        printf("CPU's turn\n");
-        choice = rand() % 3 + 1;
-    }
-    switch (choice)
-    {
-    case 1:
-        // Attack
-        attack(player, defender);
-        break;
-    case 2:
-        // Use magic
-        magic_menu(player, defender); // Pass defender to the magic_menu
-        break;
-    case 3:
-        // Defend
-        player->defense += 5;
-        printf("%s defends hp increases to: %d\n", player->name, player->hp);
-        getchar();
-        break;
-    case 4:
-        // Exit
-        printf("Player quits...\n");
-
-        printf("Exiting game...\n");
-        exit(0);
-        break;
-    default:
-        printf("Invalid choice\n");
-        break;
-    }
-}
-
-void magic_menu(Player *player, Player *defender)
-{
-    int choice;
-    if (strcmp(player->name, "CPU") == 0)
-    {
-        choice = rand() % 3 + 1;
-        player->magic[choice - 1](player, defender);
-    }
-    else
-    {
-        printf("Choose a magic attack: \n");
-        for (int i = 0; i < 3; i++)
-        {
-            printf("%d. %s\n", i + 1, *(player->magic_name + i));
-            printf("---------------------------------------------|\n");
-        }
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        // Ensure choice is valid
-        if (choice >= 1 && choice <= 3)
-        {
-            // Call the selected magic function, passing attacker and defender
-            player->magic[choice - 1](player, defender);
-        }
-        else
-        {
-            printf("Invalid choice, returning to action menu.\n");
-        }
-    }
 }
 
 int is_critical_hit(Player *defender, int dice_roll_result)
@@ -416,20 +404,32 @@ void fireball(void *attacker, void *opponent)
     Player *opponent_player = (Player *)opponent;
     Player *attacker_player = (Player *)attacker;
     int dice = dice_roll();
-    int damage = dice >= opponent_player->defense ? 20 : 40;
-    opponent_player->hp -= damage;
+    int damage = 0;
+    if (dice == 20)
+    {
+        damage = 40;
+    }
+    else if (dice >= opponent_player->defense)
+    {
+        damage = 20;
+    }
+    else
+    {
+        damage = 0;
+    }
     clear_screen();
     printf("%s prepares a fireball....\n", attacker_player->name);
     printf("\tMagic: %d. \t Defense: %d.\n", dice, opponent_player->defense);
+    getchar();
     if (dice == 20)
     {
         printf("Critical hit! %s takes 40 damage.\n", opponent_player->name);
-        opponent_player->hp -= 40;
+        opponent_player->hp -= damage;
     }
     else if (dice >= opponent_player->defense)
     {
         printf("FIREBALL HITS!!!! %s takes 20 damage.\n", opponent_player->name);
-        opponent_player->hp -= 20;
+        opponent_player->hp -= damage;
     }
     else
     {
@@ -441,7 +441,7 @@ void shield_of_light(void *attacker, void *opponent)
 {
     // Implement shield of light logic
     Player *caster = (Player *)attacker;
-    int defense_boost = 5; // Incremento temporal en defensa
+    int defense_boost = 6; // Incremento temporal en defensa
     caster->defense += defense_boost;
     clear_screen();
     printf("%s conjures a Shield of Light.\n", caster->name);
@@ -503,7 +503,8 @@ void healing(void *attacker, void *opponent)
     Player *caster = (Player *)attacker;
     int heal_amount = 10;
     caster->hp += heal_amount;
-    if (caster->hp > 100) caster->hp = 100; // Limitar la vida a 100 como máximo
+    if (caster->hp > 100)
+        caster->hp = 100; // Limitar la vida a 100 como máximo
     clear_screen();
     printf("%s casts Healing and restores %d HP!\n", caster->name, heal_amount);
     printf("%s's HP is now %d.\n", caster->name, caster->hp);
