620736554748d27c20bbe69861b1ca11ee612158
Salvador Rodriguez <chava.crb@gmail.com>
2024-09-24 08:40:23 -0600

    Modulo 2

---
+++ Module-2/malloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Ejercicios Malloc / Free 
+
+  UTILIZA MEMORIA DINAMICA PARA TODO ESTE EJERCICIO.
+
+  Regla de ORO, por cada malloc, debe de existir un FREE */
+int main()
+{
+  /* 1) crea un entero, con valor = 10 */
+  
+  /* 1.1 ... libera ese entero */
+ 
+
+  /* 2) crea un arreglo de 10 enteros */
+ 
+
+  /* 2.1) rellena ese arreglo con numeros del 1 al 10, imprimero para
+    corroborar */
+ 
+  /* 2.2 ) destruye el arreglo, ya no lo quiero */
+ 
+
+  /* 3, crea un arreglo de 20 chars, guarda en el tu nombre (pidelo de teclado) */
+ 
+
+  /* 3.1 imprimelo */
+ 
+  
+  /* 3.2 destruyelo */
+  
+
+  /* Y quien va a destruir todos los apuntadores que hemos hecho?
+    El mismo programa, porque fueron creados con memoria ESTATICA, su  
+    scope de vida (alcance) es solo el metodo main */
+
+  /* ==================================================================== */
+
+
+  
+}
